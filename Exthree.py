5ca6b575d8ff4e73c9a2ae2214ec66d39d510604
bodawalan <nishit687539@gmail.com>
2017-04-05 14:00:21 -0500

    added Exthree file

---
+++ Exthree.py
@@ -0,0 +1,17 @@
+print("I will now count my chickens:")
+
+print ("Hens",25 + 30 / 6)
+
+print ("Roosters", 100 - 25 * 3 % 4)
+
+print ("Now count eggs")
+
+print (3+2+1-5+4%2-1/4+6)
+
+print("It is true that 3+2<5 -7?")
+
+print (3+2<5 -7)
+
+print("Is it grater?",5>-2)
+print("Is it grater or equal?",5>=-2)
+print("Is it less or equal?",5<=-2)
