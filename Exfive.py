702cc6e5bbb9065f52200441b0c4fb9dac69f82d
bodawalan <nishit687539@gmail.com>
2017-04-05 15:16:39 -0500

    added Exfive

---
+++ Exfive.py
@@ -0,0 +1,12 @@
+my_name = "Nishit Bodawala"
+
+my_age = 23
+
+my_hobby = "play cricket watch movie"
+
+
+my_email = "nishit687539@gmail.com"
+
+print("my name  {} my age {} and email {}".format(my_name,my_age,my_email))
+
+
