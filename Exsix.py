8074086819207a5050fad79f04e00ec03ce55e9c
bodawalan <nishit687539@gmail.com>
2017-04-06 15:54:37 -0500

    Add files via upload

---
+++ Exsix.py
@@ -0,0 +1,17 @@
+x = "There are %d types of people." % 10
+b = "binary"
+c =" don't"
+y ="Those how we know %s and those who %s" %(b,c)
+
+print (x)
+print (y)
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print (joke_evaluation % hilarious)
+
+w = "This is the left side of..."
+e= "a string with a right side."
+
+print (w + e)
