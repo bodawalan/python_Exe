36514d2f38787bb9f1e1fd300896038b16dc40b4
bodawalan <nishit687539@gmail.com>
2017-04-06 17:31:40 -0500

    added file ten

---
+++ Exten.py
@@ -0,0 +1,13 @@
+tab_cat ="\tI'm tabbed in."
+persian_cat ="I'm split\non a line."
+bacakslach_cat="I'm \\ a \\cat."
+
+fat_cat ="""
+I'LL DO A LIST:
+\t* cat food
+\t* cat \n\t grass
+"""
+print (tab_cat)
+print (persian_cat)
+print(bacakslach_cat)
+print(fat_cat)
